980fbfa58ac2f50e91682288bc02de2f426f4909
Shalin <shalin@Shalins-MacBook-Air.local>
2023-03-22 20:23:52 +0530

    testing publish

---
+++ precache-manifest.781b601a2316dc1d46cea99aa4527220.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a83ce65b6e707552b66d",
+    "url": "/static/css/main.679dc74c.chunk.css"
+  },
+  {
+    "revision": "a83ce65b6e707552b66d",
+    "url": "/static/js/main.a83ce65b.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "62403acae71d58917074",
+    "url": "/static/js/2.62403aca.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "364b103ce7bf0bc1d82e6b40732d5fe9",
+    "url": "/static/media/logo.364b103c.png"
+  },
+  {
+    "revision": "54d29e9cea9ed4a1c2ec694874184dcc",
+    "url": "/static/media/video.54d29e9c.png"
+  },
+  {
+    "revision": "2249321a243e2d2e0dec0f9f85ce34b4",
+    "url": "/static/media/verticalLogo.2249321a.png"
+  },
+  {
+    "revision": "36d18be5182040be7abfc70ad77b49a5",
+    "url": "/index.html"
+  }
+];
